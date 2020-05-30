69b07676a3abc19444b30ff6de3788ac8a5eee46
Fraser, Vernon J <vernon_j_fraser1@homedepot.com>
2020-05-30 11:16:09 -0400

    Revised test to use argumentCaptor class

--- src/test/java/com/manors/parkview/practicalunittesting/bl/MailClientTest.java
+++ src/test/java/com/manors/parkview/practicalunittesting/bl/MailClientTest.java
@@ -1,10 +1,8 @@
 package com.manors.parkview.practicalunittesting.bl;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.validateMockitoUsage;
-import static org.mockito.Mockito.verify;
 
 import com.manors.parkview.practicalunittesting.model.Email;
 
@@ -12,6 +10,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -20,12 +19,10 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 public class MailClientTest {
 	private MailClient mailClient;
-	private Email email;
 
 	@Before
 	public void initialize(){
 		mailClient = new MailClient();
-		email = mock(Email.class);
 	}
 
 	@After
@@ -35,16 +32,20 @@ public class MailClientTest {
 
 	@Test
 	public void shouldSendMail() throws Exception {
-		String address = "123 Winter Way";
+		String address = "1234 Winter Way";
 		String title = "Blue Almonds";
 		String body = "This is a short tale";
-		PowerMockito.whenNew(Email.class).withArguments(address, title, body).thenReturn(email);
+
 		PowerMockito.mockStatic(EmailServer.class);
-		PowerMockito.doNothing().when(EmailServer.class, "sendEmail", email);
+		ArgumentCaptor<Email> captor = ArgumentCaptor.forClass(Email.class);
 
 		mailClient.sendEmail(address, title, body);
 		PowerMockito.verifyStatic(EmailServer.class, times(1));
-		EmailServer.sendEmail(email);
+		EmailServer.sendEmail(captor.capture());
+		Email emailResults = captor.getValue();
+		assertEquals(address, emailResults.getAddress());
+		assertEquals(title, emailResults.getTitle());
+		assertEquals(body, emailResults.getBody());
 	
 	}
 
